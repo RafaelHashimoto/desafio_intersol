09c00b9ddea8b64bf8eb6f04ded4be09f29688c8
Rafael <hashimotorafa@gmail.com>
2017-12-23 23:26:14 -0300

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :first_name, length:{minimum: 1, maximum:15}
-  validates :last_name, length:{minimum: 1, maximum:15}
+  validates :first_name,  presence: true, length:{maximum:15}
+  validates :last_name,   presence: true, length:{maximum:25}
+
+  has_many :accounts
+  has_many :transactions
+  
+  def full_name
+    "#{first_name} #{last_name}".strip
+  end
 end
