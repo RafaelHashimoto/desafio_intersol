62293234baa7746031acefcdade4b50bf242be70
Rafael <hashimotorafa@gmail.com>
2017-12-24 21:11:44 -0300

    controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   namespace :users do
     resources :agencies
+    resources :accounts
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
