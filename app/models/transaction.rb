b0842faf53515de103981621ff2b00c113074173
Rafael <hashimotorafa@gmail.com>
2017-12-26 21:25:32 -0300

    translation for enums

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,7 @@
 class Transaction < ApplicationRecord
+
+  include EnumTranslation
+
   before_save :set_account
   before_destroy false
 
@@ -10,7 +13,9 @@ class Transaction < ApplicationRecord
   validates :amount_in_cents   ,presence: true
   validates :date              ,presence: true
   validates :user_id           ,presence: true 
-  
+  validate  :account_uniqueness
+  validate  :amount_limit,  if: :validation_for_origin_account
+
   enum transaction_type: [:deposit, :withdrawal, :transfer, :chargeback]
 
   belongs_to :origin_account  , class_name: "Account",
@@ -29,6 +34,18 @@ class Transaction < ApplicationRecord
     elsif transfer?
       origin_account_id = origin_account
       destination_account_id = destination_account
+    end
+  end
+
+  def account_uniqueness
+    if origin_account_id == destination_account_id
+      errors.add(:destination_account_id, 'não pode ser igual a conta de origem')
+    end
+  end
+
+  def amount_limit
+    if amount_in_cents.to_i > Account.find(origin_account_id).limit.to_i
+      errors.add(:amount_in_cents, 'não pode ser maior que o limite da conta')
     end
   end
 
