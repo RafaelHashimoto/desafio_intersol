66e880455a172a42ab210b209fb823e85a5755c5
Rafael <hashimotorafa@gmail.com>
2017-12-26 21:26:34 -0300

    helpers for view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,41 @@
 module ApplicationHelper
+  
+  def agencies?
+    Agency.all.any?
+  end
 
+  def accounts?
+    Account.where(user_id: current_user.id ).any?
+  end
+
+  def transactions?
+    Transaction.where(user_id: current_user.id).any?
+  end
+
+  def options_for_transaction_type
+    ['deposit', 'withdrawal', 'transfer']
+  end
+
+  def my_accounts
+    Account.where(user_id: current_user.id)
+  end
+
+  def accounts
+    Account.all
+  end
+
+  def transaction_amount(transaction, account)
+    if transaction.deposit? || (transaction.transfer? && transaction.destination_account == account) 
+      " + " + number_to_currency((transaction.amount_in_cents.to_f/100), unit: "R$") 
+    elsif transaction.withdrawal? || (transaction.transfer? && transaction.origin_account == account) 
+      " - " + number_to_currency((transaction.amount_in_cents.to_f/100), unit: "R$") 
+    elsif transaction.chargeback?
+      if (transaction.try(:origin_account) == account) 
+        " + " + number_to_currency((transaction.amount_in_cents.to_f/100), unit: "R$") 
+      elsif (transaction.try(:destination_account) == account) 
+        " - " + number_to_currency((transaction.amount_in_cents.to_f/100), unit: "R$") 
+      end
+    end
+  end
+  
 end
